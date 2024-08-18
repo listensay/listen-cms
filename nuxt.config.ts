392e2da5fc3c5b406c17c6e0e670b36ea81377c4
codemmk <2107228359@qq.com>
2024-08-18 20:05:32 +0800

    prisma init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import Aura from '@primevue/themes/aura'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module'],
+  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module', '@prisma/nuxt'],
   css: [
     '@/assets/css/style.css'
   ],
