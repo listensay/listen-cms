79b4683d5a8bfa02192ddcf88fd911f6de293bac
Miliya <odr233@gmail.com>
2024-08-19 11:10:14 +0800

    check password

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,15 @@ module.exports = {
   rules: { // 自訂規則
     quotes: ['error', 'single'], // 使用單引號
     'no-console': 'warn', // 使用 console 時警告提示
-    'vue/multi-word-component-names': 'off'
-  }
+    'vue/multi-word-component-names': 'off',
+    'semi': ['error', 'never'] // 禁止使用分号
+  },
+  overrides: [
+    {
+      files: ['./server/**/*.ts'],  // 路径根据你的项目结构调整
+      rules: {
+        'no-console': 'off'  // 关闭 no-console 规则
+      }
+    }
+  ]
 }
