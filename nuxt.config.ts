0f5c30868f0f00f7d4e3d6bf4675578b026f57e7
Miliya <odr233@gmail.com>
2024-08-19 15:18:54 +0800

    token 密钥设置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,8 @@ export default defineNuxtConfig({
   // 全局路由过渡效果
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
-  }
+  },
+  runtimeConfig: {
+    SECRET_KEY: process.env.SECRET_KEY
+  },
 })
