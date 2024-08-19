a7ade725e7a6c2c886020d13ecc05277614392e2
Miliya <odr233@gmail.com>
2024-08-19 14:32:29 +0800

    数据验证工具封装

---
+++ server/utils/useValidate.ts
@@ -0,0 +1,21 @@
+// joi 校验封装
+import joi from 'joi'
+
+/**
+ * 数据校验
+ * @param body 验证对象
+ * @param options joi验证参数
+ * @returns boolean值
+ */
+const useValidate = async (body: any, options: any) => {
+  const schema = joi.object({ ...options })
+
+  try {
+    await schema.validateAsync(body)
+    return true
+  } catch {
+    return false
+  }
+}
+
+export default useValidate
