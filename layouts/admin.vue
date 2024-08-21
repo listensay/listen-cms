96fee370dfdabf6f059ef00114a5fe771dfdf224
Miliya <odr233@gmail.com>
2024-08-21 11:18:19 +0800

    updated: 定义菜单数据

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,10 +1,48 @@
 <script setup>
+const items = ref([
+  {
+      separator: true
+  },
+  {
+      label: '基本管理',
+      items: [
+          {
+              label: '仪表盘',
+              icon: 'pi pi-chart-pie',
+              url: '/admin/dashboard'
+          },
+          {
+              label: '文章管理',
+              icon: 'pi pi-book',
+              url: '/admin/article'
+          },
+          {
+            label: '网站首页',
+              icon: 'pi pi-home',
+              url: '/'
+          },
+      ]
+  },
+  {
+      label: '网站管理',
+      items: [
+          {
+              label: '网站设置',
+              icon: 'pi pi-cog',
+          },
+          {
+              label: '消息',
+              icon: 'pi pi-inbox',
+          }
+      ]
+  }
+])
 </script>
 
 <template>
   <div class="layout bg-slate-200">
     <div class="flex p-8 h-[100vh]">
-      <LayoutMenu />
+      <LayoutMenu :tabs="items"/>
       <div class="flex-1 ml-4">
         <div class="bg-white shadow-md rounded-md h-full">
           <slot />
