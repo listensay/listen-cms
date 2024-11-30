906956b4fa77fe6fa3f34667c7fe6f627978134d
codemmk <2107228359@qq.com>
2024-11-30 19:35:33 +0800

    布局修改

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,34 @@
 <script lang="ts" setup>
-
+const tabs = [
+  {
+      label: '基本管理',
+      id: 1,
+      items: [
+        {
+            label: 'Home',
+            icon: 'pi pi-chart-pie',
+            url: '/'
+        },
+        {
+          label: 'About',
+            icon: 'pi pi-home',
+            url: '/about'
+        },
+      ]
+  }
+]
 </script>
 
 <template>
-  <div class="layout h-full">
-    <slot />
+  <div class="home h-[100vh] p-8 w-full">
+    <div class="flex h-full">
+      <div class="w-64 mr-4">
+        <AppMenu :tabs="tabs" :border="false" class="shadow-md" />
+      </div>
+      <div class="flex-1">
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
 
