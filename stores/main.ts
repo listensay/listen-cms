548d4d46ed5ff19e77d13b4321f706dd335efb1a
codemmk <2107228359@qq.com>
2024-11-30 19:35:52 +0800

    代码改进

--- stores/main.ts
+++ stores/main.ts
@@ -1,10 +1,8 @@
-const token = useCookie('token')
-const router = useRouter()
-
 const useMainStore = defineStore('main', () => {
+  const token = useCookie('token')
+  const router = useRouter()
   const haslogin = ref(false)
   const userinfo = ref(null)
-  
 
   if (token.value) {
     haslogin.value = true
@@ -15,17 +13,25 @@ const useMainStore = defineStore('main', () => {
   }
 
   async function fetchGetUserInfo() {
-    const result = await useFetchUserProfile().auth.getUserInfo()
-    userinfo.value = result
+    try {
+      const result = await useFetchUserProfile().auth.getUserInfo()
+      userinfo.value = result
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async function fetchChangeUserInfo(data: userinfo) {
-    const result = await useFetchUserProfile().auth.updateUserInfo(data)
-    if(result.statusCode === 200) {
-      haslogin.value = false
-      token.value = null
-      message.success('修改成功, 请重新登陆 : ) ')
-      router.push('/login?from=/admin/profile')
+    try {
+      const result = await useFetchUserProfile().auth.updateUserInfo(data)
+      if(result.statusCode === 200) {
+        haslogin.value = false
+        token.value = null
+        message.success('修改成功, 请重新登陆 : ) ')
+        router.push('/login?from=/admin/profile')
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
