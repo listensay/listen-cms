d73075e440f51e1d96aba66b3d90f0ec91e74b20
codemmk <2107228359@qq.com>
2024-11-30 19:33:35 +0800

    文章格式

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+// tailwind.config.js
+import typography from '@tailwindcss/typography'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    typography,
+  ]
 }
